5fa4f6583aea643cf18d82a05b84c252dd88e85a
Rashmi Bora <you@example.com>
2022-07-07 17:47:36 +0530

    initialt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'templatedriven';
-  loginUser(formData:string){
+  loginUser(formData: string) {
     console.log(formData);
   }
 }
